b8a4bb230adb91c26d70a09be62f3453b091583f
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-03-03 21:46:46 +0530

    Non-responsive menu overlay complete

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Inital State of Menu
 let showMenu = false;
